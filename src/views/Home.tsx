73fe24a810bc0c1558c383c1ed398678ea2668c3
Hirwa Blessing <hirwablessing7@gmail.com>
2021-08-08 16:43:39 +0200

    Add expense component,change card's titles

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -7,9 +7,9 @@ export default function Home() {
   return (
     <Dashboard title="Expenses" subtitle="Statistics">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mb-10 p-5 lg:p-0">
+        <Card total={345} title="Incomes" img="" bgColor="#ec6448" />
         <Card total={345} title="Expenses" img="" bgColor="#ec6448" />
-        <Card total={345} title="Expenses" img="" bgColor="#ec6448" />
-        <Card total={345} title="Income" img="" bgColor="#ec6448" />
+        <Card total={345} title="Total Transactions" img="" bgColor="#ec6448" />
       </div>
       <div className="bg-white rounded shadow-md my-2 w-full pb-10">
         <IncomeChart />
