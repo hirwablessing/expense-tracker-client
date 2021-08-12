536cefd63e458e34d842777c286bff4afe2cc16a
Blessing Hirwa <50826640+hirwablessing@users.noreply.github.com>
2021-08-12 17:17:11 +0200

    Remove unwanted comments

--- src/components/ExpenseChart.tsx
+++ src/components/ExpenseChart.tsx
@@ -6,11 +6,6 @@ import {
   getTotalIncomeByMonth,
 } from "../services/all.service";
 
-// const globalAny: any = global;
-
-// globalAny.global.tooltips.enabled = false;
-// globalAny.global.legend.position = "bottom";
-
 export default function ExpenseChart() {
   const [totalExpense, setExpenses] = useState([]);
   const [highExpense, setHighExpense] = useState("");
@@ -73,13 +68,7 @@ export default function ExpenseChart() {
                   "rgba(255, 159, 64, 1)",
                 ],
                 borderWidth: 1,
-              },
-              // {
-              //   label: 'Quantity',
-              //   data: [47, 52, 67, 58, 9, 50],
-              //   backgroundColor: 'orange',
-              //   borderColor: 'red',
-              // },
+              }
             ],
           }}
           height={400}
